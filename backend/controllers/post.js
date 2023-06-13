aba991bd230e9db656699754634cadc767aa1c7e
Biruh <lebiruh@yahoo.com>
2023-06-13 16:04:51 +0300

    added authentication before getting and adding posts

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -3,23 +3,22 @@ import jwt from 'jsonwebtoken';
 import moment from 'moment';
 
 export const getPosts = (req, res) => {
-  // const token = req.cookies.accessToken;
 
-  // if (!token) return res.status(401).json("Not logged in");
+  const token = req.cookies.accessToken;
 
-  // jwt.verify(token, process.env.JWT_SECRET, (err, userInfo) => {
-  //   if(err) return res.status(403).json("Token is invalid!")
-  // })
+  if (!token) return res.status(401).json("Not logged in");
 
-  //TODO: handle user authentication before fetching postMessage.
+  jwt.verify(token, process.env.JWT_SECRET, (err, userInfo) => {
+    if(err) return res.status(403).json("Token is invalid!")
+    const q = "SELECT p.*, u.id AS userId, firstName FROM posts AS p JOIN users AS u ON (p.userId = u.id)"
 
-  const q = "SELECT p.*, u.id AS userId, firstName FROM posts AS p JOIN users AS u ON (p.userId = u.id)"
+    db.query(q, (err, data) => { 
+      if (err) return res.status(500).json(err);
+      // console.log(data);
+      return res.status(200).json(data);
+    });
+  })
 
-  db.query(q, (err, data) => { 
-    if (err) return res.status(500).json(err);
-    // console.log(data);
-    return res.status(200).json(data);
-  });
 };
 
 export const addPost = (req, res) => {
@@ -29,8 +28,7 @@ export const addPost = (req, res) => {
 
   jwt.verify(token, process.env.JWT_SECRET, (err, userInfo) => {
     if(err) return res.status(403).json("Token is invalid!")
-    //TODO: handle user authentication before fetching postMessage.
-
+  
     const q = "INSERT INTO posts (`userId`, `content`, `image`, `createdAt`) VALUES (?)"
 
     const values = [
@@ -42,7 +40,7 @@ export const addPost = (req, res) => {
 
     db.query(q, [values], (err, data) => { 
       if (err) return res.status(500).json(err);
-      console.log(data);
+      // console.log(data);
       return res.status(200).json("Post has been created");
     });
   });  
