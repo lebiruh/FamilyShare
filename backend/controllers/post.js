33d91071ebf9f80fa56911db7a59487f4e1e5c5a
Biruh <lebiruh@yahoo.com>
2023-06-13 11:45:07 +0300

    end points to add and get posts created

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -1,3 +1,50 @@
+import {db} from "../connectDB.js";
+import jwt from 'jsonwebtoken';
+import moment from 'moment';
+
 export const getPosts = (req, res) => {
-  res.send("Hello, world from posts!");
-}
+  // const token = req.cookies.accessToken;
+
+  // if (!token) return res.status(401).json("Not logged in");
+
+  // jwt.verify(token, process.env.JWT_SECRET, (err, userInfo) => {
+  //   if(err) return res.status(403).json("Token is invalid!")
+  // })
+
+  //TODO: handle user authentication before fetching postMessage.
+
+  const q = "SELECT p.*, u.id AS userId, firstName FROM posts AS p JOIN users AS u ON (p.userId = u.id)"
+
+  db.query(q, (err, data) => { 
+    if (err) return res.status(500).json(err);
+    // console.log(data);
+    return res.status(200).json(data);
+  });
+};
+
+export const addPost = (req, res) => {
+  const token = req.cookies.accessToken;
+
+  if (!token) return res.status(401).json("Not logged in");
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, userInfo) => {
+    if(err) return res.status(403).json("Token is invalid!")
+    //TODO: handle user authentication before fetching postMessage.
+
+    const q = "INSERT INTO posts (`userId`, `content`, `image`, `createdAt`) VALUES (?)"
+
+    const values = [
+      userInfo.id,
+      req.body.content,
+      req.body.image,
+      moment(Date.now()).format("YYYY-MM-DD HH:mm:ss")
+    ]
+
+    db.query(q, [values], (err, data) => { 
+      if (err) return res.status(500).json(err);
+      console.log(data);
+      return res.status(200).json("Post has been created");
+    });
+  });  
+
+};
